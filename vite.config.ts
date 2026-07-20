28e4ff1242d433df7aaa092c911f0afd6aa9e77c
zty1214 <148088653+zty1214@users.noreply.github.com>
2026-07-20 17:54:14 +0800

    update: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/wedding-planner/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
